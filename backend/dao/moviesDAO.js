5b0cfbf1b6908f377e9176d174e88514907639fe
AnishBade <badeanish6045@gmail.com>
2023-07-14 09:01:21 +0545

    data fetch wrong

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -5,7 +5,8 @@ export default class MoviesDAO{
             return
         }
         try{
-            movies = await conn.db(process.env.MOVIESREVIEW_NS).collection('movies')
+            movies = await conn.db(process.env.sample_mfilx)
+            .collection('movies')
         }
         catch(e){
             console.error(`unable to connect in MoviesDAO: ${e}`)
@@ -19,26 +20,26 @@ export default class MoviesDAO{
         } = {}){
         let query
         if(filters){
-        if("title" in filters){
-        query = { $text: { $search: filters['title']}}
-        }else if("rated" in filters){
-        query = { "rated": { $eq: filters['rated']}}
-        }
+            if("title" in filters){
+                query = { $text: { $search: filters['title']}}
+            }else if("rated" in filters){
+                query = { "rated": { $eq: filters['rated']}}
+            }
         }
         let cursor
         try{
-        cursor = await movies
-        .find(query)
-        .limit(moviesPerPage)
-        .skip(moviesPerPage * page)
-        const moviesList = await cursor.toArray()
-        const totalNumMovies = await movies.countDocuments(query)
-        return {moviesList, totalNumMovies}
+                cursor = await movies
+                .find(query)
+                .limit(moviesPerPage)
+                .skip(moviesPerPage * page)
+            const moviesList = await cursor.toArray()
+            const totalNumMovies = await movies.countDocuments(query)
+            return {moviesList, totalNumMovies}
         }
         catch(e){
-        console.error(`Unable to issue find command, ${e}`)
-        return { moviesList: [], totalNumMovies: 0}
+            console.error(`Unable to issue find command, ${e}`)
+            return { moviesList: [], totalNumMovies: 0}
         }
-        }
+    }
 }
 
